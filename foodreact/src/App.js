87f9df6ed6add3252376c259dfbe6151eb9e9fd9
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-04-27 12:06:28 +0200

    update app.js

--- foodreact/src/App.js
+++ foodreact/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import Card from "./components/Card";
@@ -15,22 +15,44 @@ function App() {
 
   const getData = () => {
     fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_API_KEY}?ingredients=${value}`
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_API_KEY}&ingredients=${value}`
     )
       .then((res) => res.json())
       .then((json) => {
         setItems(json);
-
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
       });
+    .then((
+      var string = JSON.stringify(items);
+      var parsed = JSON.parse(string);
+      console.log(parsed);
+    )
+     
+    );
   };
+  // for (const [key, value] of Object.entries(items)) {
+  //   console.log(`${key}: ${value}`);
+  // }
+
+  // const map = Object.entries(items);
+
+  // var response = JSON.map(items);
+  // console.log(response);
+
+  // Object.keys(items).forEach(function (key) {
+  //   console.log("Key : " + key + ", Value : " + items[key]);
+  // });
+
+  // items.forEach((obj) => {
+  //   Object.entries(obj).forEach(([key, value]) => {
+  //     console.log(`${key} ${value}`);
+  //   });
+  //   console.log("-------------------");
+  // });
 
   return (
     <div className="App">
-      <h1> Fetch data from an api in react </h1> {console.log(items)}
+      <h1> Fetch data from an api in react </h1>
+      
       <Button value="hummus" onClick={save}>
         Hummus
       </Button>
