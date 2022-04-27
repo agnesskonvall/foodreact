e243d63045d33d1eabb379286477167641ec421a
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-04-27 09:41:32 +0200

    added ingredients + states

--- foodreact/src/App.js
+++ foodreact/src/App.js
@@ -1,46 +1,46 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState(null);
 
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
+  const save = (e) => {
+    e.preventDefault();
+    setValue(e.target.value);
+  };
 
-  componentDidMount() {
+  const getData = () => {
     fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_API_KEY}?ingredients=${value}`
     )
       .then((res) => res.json())
       .then((json) => {
+        setItems(json);
+
         this.setState({
           items: json,
           DataisLoaded: true,
         });
       });
-  }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Pleses wait some time.... </h1>{" "}
-        </div>
-      );
+  };
 
-    return (
-      <div className="App">
-        <h1> Fetch data from an api in react </h1> {console.log(items)}
-        <Button>Normal Button</Button>
-        <Button primary>Primary Button</Button>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1> Fetch data from an api in react </h1> {console.log(items)}
+      <Button value="hummus" onClick={save}>
+        Hummus
+      </Button>
+      <Button value="chocolate" onClick={save}>
+        Chocolate
+      </Button>
+      <Button primary onClick={getData}>
+        Get recipes
+      </Button>
+    </div>
+  );
 }
 
 export default App;
