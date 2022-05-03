c51ae0d4c7de6d5cab7a4e1d302a9f55e696a9c0
alicenyberg <alice.nyberg@hotmail.com>
2022-05-03 21:50:20 +0200

    some cleanup

--- foodreact/src/components/Text/index.js
+++
@@ -1,5 +0,0 @@
-import { TitleStyled } from "./styles";
-
-const Title = TitleStyled;
-
-export default Title;
--- foodreact/src/components/Text/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const TitleStyled = styled.h1`
-  font-size: 30px;
-  font-family: monospace;
-  color: black;
-`;
